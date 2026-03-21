0579f5a7a4e43f984a6b8df303771900caa6d179
animesh-94 <animeshy068@gmail.com>
2026-03-21 23:26:35 +0530

    resolving the deployment issue

--- proxy.ts
+++ proxy.ts
@@ -16,7 +16,8 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
+    // Standard Next.js/Clerk matcher
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
   ],
 };
