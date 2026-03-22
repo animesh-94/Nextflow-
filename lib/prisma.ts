3280f9c225ba672f74f32e7428161c459dbc2bd7
animesh-94 <animeshy068@gmail.com>
2026-03-22 10:52:12 +0530

    fix: final migration to Prisma 7 configuration

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,7 +2,8 @@ import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
   return new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL, // Pass the URL here instead
+    // Pass the environment variable here
+    datasourceUrl: process.env.DATABASE_URL,
   })
 }
 
