64b3baf9676ea120fe529088abc467b32577800d
animesh-94 <animeshy068@gmail.com>
2026-03-22 12:52:46 +0530

    added 6

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,18 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
 const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
+    adapter,
   });
 
 if (process.env.NODE_ENV !== "production") {
